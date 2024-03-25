c288a88e7a56cc0392aebed78ae33a6cd1930588
CLINTON <issnasty2@gmail.com>
2024-03-25 09:00:39 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+
+
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+function appendCat(name) {
+    const Figaro=[...cats,name]
+    return Figaro
+}
+function prependCat(name) {
+    const molly=[name,...cats]
+    return molly
+}
+function removeLastCat(name) {
+    const Kinuthia=cats.slice(0,-1);
+    return Kinuthia
+}
+function removeFirstCat(params) {
+   
+        const Aliya=cats.slice(1)
+        return Aliya
+    }
+    
+
+
